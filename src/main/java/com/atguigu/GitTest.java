11c69f8b6c82440659335509d81d29533f48974a
hujie <919206040@qq.com>
2022-05-29 19:30:42 +0800

    push commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Hello,Java");
         System.out.println("master test");
         System.out.println("Hot-fix test");
+        System.out.println("push commit");
     }
 }
