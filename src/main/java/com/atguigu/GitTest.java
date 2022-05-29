7f89ec7a685611227c213391714b9a0907adc443
hujie <919206040@qq.com>
2022-05-29 19:06:19 +0800

    second commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("Hello,Git!");
         System.out.println("Hello,World");
         System.out.println("Hello,man");
+        System.out.println("Hello,Java");
     }
 }
